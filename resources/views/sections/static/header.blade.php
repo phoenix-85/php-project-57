4be13a76af11849736f6f4a8fbfa038326919155
phoenix <phoenix-85@github.com>
2025-06-17 23:06:09 +0500

    step 6

--- resources/views/sections/static/header.blade.php
+++ resources/views/sections/static/header.blade.php
@@ -28,16 +28,14 @@
                         onclick="location.href='{{ route('login') }}';"
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     >
-                        Войти
-{{--                        {{ __('Log in') }}--}}
+                        {{ __('Log in') }}
                     </button>
                     <button
                         type="button"
                         onclick="location.href='{{ route('register') }}';"
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
                     >
-                        Зарегистрировать
-{{--                        {{ __('Register') }}--}}
+                        {{ __('Register') }}
                     </button>
 {{--                    <a--}}
 {{--                        href="{{ route('login') }}"--}}
