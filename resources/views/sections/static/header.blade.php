8894dfb6c4b2f1474d37190f2cd5ddfc8b67dbfc
phoenix <phoenix-85@github.com>
2025-06-17 22:48:31 +0500

    step 6

--- resources/views/sections/static/header.blade.php
+++ resources/views/sections/static/header.blade.php
@@ -23,18 +23,20 @@
                         </button>
                     </form>
                 @else
-                    <input
+                    <button
                         type="button"
                         onclick="location.href='{{ route('login') }}';"
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        value="{{ __('Log in') }}"
-                    />
-                    <input
+                    >
+                        {{ __('Log in') }}
+                    </button>
+                    <button
                         type="button"
                         onclick="location.href='{{ route('register') }}';"
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
-                        value="{{ __('Register') }}"
-                    />
+                    >
+                        {{ __('Register') }}
+                    </button>
 {{--                    <a--}}
 {{--                        href="{{ route('login') }}"--}}
 {{--                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"--}}
