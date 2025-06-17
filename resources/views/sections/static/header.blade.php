7cccf3b95460470c487ba9ce05095230be3d0c11
phoenix <phoenix-85@github.com>
2025-06-17 22:42:24 +0500

    step 6

--- resources/views/sections/static/header.blade.php
+++ resources/views/sections/static/header.blade.php
@@ -23,18 +23,30 @@
                         </button>
                     </form>
                 @else
-                    <a
-                        href="{{ route('login') }}"
+                    <input
+                        type="button"
+                        onclick="location.href='{{ route('login') }}';"
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    >
-                        {{ __('Log in') }}
-                    </a>
-                    <a
-                        href="{{ route('register') }}"
+                        value="{{ __('Log in') }}"
+                    />
+                    <input
+                        type="button"
+                        onclick="location.href='{{ route('register') }}';"
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
-                    >
-                        {{ __('Register') }}
-                    </a>
+                        value="{{ __('Register') }}"
+                    />
+{{--                    <a--}}
+{{--                        href="{{ route('login') }}"--}}
+{{--                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"--}}
+{{--                    >--}}
+{{--                        {{ __('Log in') }}--}}
+{{--                    </a>--}}
+{{--                    <a--}}
+{{--                        href="{{ route('register') }}"--}}
+{{--                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"--}}
+{{--                    >--}}
+{{--                        {{ __('Register') }}--}}
+{{--                    </a>--}}
                 @endauth
             </div>
             <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
