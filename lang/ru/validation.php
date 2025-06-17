e9bed82803c82d0413e224e19f0422e965ba4890
phoenix <phoenix-85@github.com>
2025-06-17 23:41:37 +0500

    step 6

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -107,7 +107,7 @@ return [
         'array' => 'The :attribute field must have at least :min items.',
         'file' => 'The :attribute field must be at least :min kilobytes.',
         'numeric' => 'The :attribute field must be at least :min.',
-        'string' => 'The :attribute field must be at least :min characters.',
+        'string' => ':attribute должен иметь длину не менее :min символов.',
     ],
     'min_digits' => 'The :attribute field must have at least :min digits.',
     'missing' => 'The :attribute field must be missing.',
--- resources/views/sections/static/header.blade.php
+++ resources/views/sections/static/header.blade.php
@@ -13,14 +13,15 @@
             </a>
             <div class="flex items-center lg:order-2">
                 @auth
-                    <form method="POST" action="{{ route('logout') }}">
+                    <a
+                        href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none">
                         @csrf
-                        <button
-                            type="submit"
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        >
-                            {{ __('Logout') }}
-                        </button>
                     </form>
                 @else
                     <button
@@ -37,18 +38,6 @@
                     >
                         {{ __('Register') }}
                     </button>
-{{--                    <a--}}
-{{--                        href="{{ route('login') }}"--}}
-{{--                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"--}}
-{{--                    >--}}
-{{--                        {{ __('Log in') }}--}}
-{{--                    </a>--}}
-{{--                    <a--}}
-{{--                        href="{{ route('register') }}"--}}
-{{--                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"--}}
-{{--                    >--}}
-{{--                        {{ __('Register') }}--}}
-{{--                    </a>--}}
                 @endauth
             </div>
             <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
