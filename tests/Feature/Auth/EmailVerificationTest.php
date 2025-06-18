c0f267557bd87d88ac93f394e67bef45299e3424
phoenix <phoenix-85@github.com>
2025-06-18 21:59:53 +0500

    step 6

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -38,7 +38,7 @@ class EmailVerificationTest extends TestCase
 
         Event::assertDispatched(Verified::class);
         $this->assertTrue($user->fresh()->hasVerifiedEmail());
-        $response->assertRedirect(route('main', absolute: false).'?verified=1');
+        $response->assertRedirect(route('main', absolute: false) . '?verified=1');
     }
 
     public function testEmailIsNotVerifiedWithInvalidHash(): void
